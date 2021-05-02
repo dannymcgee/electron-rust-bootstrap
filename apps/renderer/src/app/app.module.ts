d24da923381c991699dfb37bced4d5feccb0316e
Danny McGee <dannymcgee@gmail.com>
2021-05-01 22:24:50 -0400

    Generate Electron project

--- apps/renderer/src/app/app.module.ts
+++ apps/renderer/src/app/app.module.ts
@@ -4,9 +4,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-	declarations: [AppComponent],
 	imports: [BrowserModule],
-	providers: [],
+	declarations: [AppComponent],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
