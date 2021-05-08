87950c46c7f4b3a4f1410dafbab92367c7d6b655
Danny McGee <dannymcgee@gmail.com>
2021-05-08 18:25:49 -0400

    Fix failing test

--- apps/renderer/src/app/app.component.spec.ts
+++ apps/renderer/src/app/app.component.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from "@angular/core/testing";
+import { createComponentFactory, Spectator } from "@ngneat/spectator/jest";
+
 import { AppComponent } from "./app.component";
+import { ElectronService } from "./electron.service";
 
 describe("AppComponent", () => {
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [AppComponent],
-		}).compileComponents();
+	let spectator: Spectator<AppComponent>;
+	const createComponent = createComponentFactory({
+		component: AppComponent,
+		mocks: [ElectronService],
+	});
+
+	beforeEach(() => {
+		spectator = createComponent();
 	});
 
 	it("should create the app", () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app).toBeTruthy();
+		expect(spectator.component).toExist();
 	});
 });
