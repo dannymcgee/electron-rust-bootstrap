b2e5533be66db23623f8e54baef78858d6192ffd
Danny McGee <dannymcgee@gmail.com>
2021-05-31 17:47:13 -0400

    Update protobuf builder to rebuild Rust types

--- tools/builders/protobuf/build/impl.ts
+++ tools/builders/protobuf/build/impl.ts
@@ -1,5 +1,7 @@
 import { promises as fs } from "fs";
+import * as cp from "child_process";
 import * as path from "path";
+import * as _rimraf from "rimraf";
 
 import { BuilderContext, createBuilder } from "@angular-devkit/architect";
 
@@ -14,9 +16,24 @@ async function runBuilder(opts: Options, ctx: BuilderContext) {
 	let workspaceJson = await fs
 		.readFile(path.join(workspaceRoot, "angular.json"))
 		.then(buf => JSON.parse(buf.toString()));
-
 	let srcRoot = workspaceJson?.projects?.[ctx.target.project]?.sourceRoot as string;
 
+	try {
+		await Promise.all([
+			generateTsFiles(protosPath, srcRoot),
+			buildRustLib(srcRoot),
+		]);
+	} catch (err) {
+		return {
+			success: false,
+			error: err.stack,
+		};
+	}
+
+	return { success: true };
+}
+
+async function generateTsFiles(protosPath: string, srcRoot: string) {
 	let filenames = (await fs.readdir(protosPath))
 		.filter(f => f.endsWith(".proto"));
 
@@ -34,6 +51,30 @@ async function runBuilder(opts: Options, ctx: BuilderContext) {
 
 		await fs.writeFile(outPath, transpiled);
 	}));
+}
 
-	return { success: true };
+async function buildRustLib(srcRoot: string) {
+	let workingDir = path.resolve(srcRoot, "lib/rust");
+	let prevOutput = path.resolve(workingDir, "target");
+
+	await rimraf(prevOutput);
+
+	return new Promise<void>((resolve, reject) => {
+		cp.spawn("cargo", ["build"], {
+			stdio: "inherit",
+			cwd: workingDir,
+		})
+			.on("close", resolve)
+			.on("exit", resolve)
+			.on("error", reject);
+	});
+}
+
+async function rimraf(dir: string) {
+	return new Promise<void>((resolve, reject) => {
+		_rimraf(dir, (err) => {
+			if (err) reject(err);
+			else resolve();
+		});
+	});
 }
